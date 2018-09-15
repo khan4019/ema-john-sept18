73b891e0fe6d1f22c791144b02d47e8981539d89
That JS Dude <khan4019@gmail.com>
2018-09-15 12:39:38 -0500

    first commit

---
+++ ema-john-sept18/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ema-john-sept18/src/Header/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.png';
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <img src={logo} alt=""/>
+                <p className="menu">
+                    <a href="/shop">Shop</a>
+                    <a href="/review">Review</a>
+                    <a href="/orders">Orders</a>
+                </p>
+            </div>
+        );
+    }
+}
+
+export function abc (){
+    
+}
+
+export default Header;
